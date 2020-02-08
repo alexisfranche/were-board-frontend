0f9234fb8e1ca9b96a1176253a7436b2da43650a
Nima <nima.u.chatlani@gmail.com>
2020-02-08 17:37:18 -0500

    added logout tab

--- were_board/lib/home_tabs.dart
+++ were_board/lib/home_tabs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getflutter/getflutter.dart';
+import 'package:were_board/login.dart';
 
 import 'view_profile.dart';
 
@@ -11,7 +12,7 @@ class HomePage extends StatelessWidget{
           tabBarHeight: 85.0,
           tabBarColor: Colors.lightBlueAccent,
           initialIndex: 0,
-          length: 4,
+          length: 5,
           tabs: <Widget>[
             Tab(
               icon: Icon(Icons.casino),
@@ -32,6 +33,12 @@ class HomePage extends StatelessWidget{
                 ),
               ),
               Tab(
+                icon: Icon(Icons.power),
+                child: Text(
+                  "Logout",
+                ),
+              ),
+              Tab(
                 icon: Icon(Icons.settings),
                 child: Text(
                   "Settings",
@@ -46,6 +53,7 @@ class HomePage extends StatelessWidget{
                //ViewProfile(), // Un-comment to test view profile page
                Container(child: Icon(Icons.event_available), color: Colors.white,),
                Container(child: ViewProfile()),
+               Container(child: LoginScreen(), ),
                Container(child: Icon(Icons.settings), color: Colors.white,),
              ],
             ),
